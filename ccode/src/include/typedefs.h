d2dc7433174e6fce72feb93cb77a5e1d7add0f7b
sverrkva <sverre.kvamme@gmail.com>
2014-12-12 18:12:57 +0200

    update

--- ccode/src/include/typedefs.h
+++ ccode/src/include/typedefs.h
@@ -8,25 +8,8 @@
 #ifndef TYPEDEFS_H_
 #define TYPEDEFS_H_
 
-/* MISRA C 2004 compliant numeric typedef */
 
 typedef char char_t;
-
-///* Probably already defined by inttypes.h or types.h*/
-//# ifndef __int8_t_defined
-//#  define __int8_t_defined
-//typedef signed char int8_t;
-//typedef signed short int16_t;
-//typedef signed int int32_t;
-//#endif
-//
-//#ifndef _SYS_TYPES_H
-//#if !defined(_INT64_T) && !defined(INT64_MAX)
-//#define _INT64_T
-//typedef signed long int64_t;
-//#endif
-//#endif
-
 
 typedef signed char int8_t;
 typedef signed short int16_t;
